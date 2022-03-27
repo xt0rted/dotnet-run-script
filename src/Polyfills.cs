3126dc6200213ea6ba4f60b231db59fa2970c447
Brian Surowiec <brian@13degrees.com>
2022-03-27 01:10:03 -0400

    Initial pass

---
+++ src/Polyfills.cs
@@ -0,0 +1,13 @@
+#if NETCOREAPP3_1
+
+namespace System.Runtime.CompilerServices
+{
+    using System.ComponentModel;
+
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal static class IsExternalInit
+    {
+    }
+}
+
+#endif
